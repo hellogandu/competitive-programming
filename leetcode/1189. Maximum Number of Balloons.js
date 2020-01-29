24b286311cd4862611ccaf6ed35a6de59cfa7758
reettik <reettikgoswami1997@gmail.com>
2020-01-29 21:33:58 +0530

    1189. Maximum Number of Balloons

---
+++ leetcode/1189. Maximum Number of Balloons.js
@@ -0,0 +1,49 @@
+// Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+
+// You can use each character in text at most once. Return the maximum number of instances that can be formed.
+
+// Example 1:
+
+// Input: text = "nlaebolko"
+// Output: 1
+
+// Example 2:
+
+// Input: text = "loonbalxballpoon"
+// Output: 2
+
+// Example 3:
+
+// Input: text = "leetcode"
+// Output: 0
+
+// /**
+//  * @param {string} text
+//  * @return {number}
+//  */
+var maxNumberOfBalloons = function(text) {
+  let b = 0;
+  let a = 0;
+   let l =0;
+   let o =0;
+   let n = 0;
+   text = text.toLowerCase();
+ for(let i=0;i<text.length;i++){
+     switch(text[i]){
+         case "a" : a+=1;
+                    break;
+         case "b" : b+=1;
+                    break;
+         case "l" : l+=1;
+                    break;
+         case "o" : o+=1;
+                    break;
+         case "n" : n+=1;
+                    break;
+       default: break;
+            }
+ }
+     l = Math.floor(l/2);
+     o = Math.floor(o/2);
+     return Math.min(a,b,l,o,n);
+};
