2465ca7bc994772a5b6c4d75f8e380a147186ea7
reettik <reettikgoswami1997@gmail.com>
2020-05-08 15:15:43 +0530

    1232. Check If It Is a Straight Line

---
+++ leetcode/1232. Check If It Is a Straight Line.js
@@ -0,0 +1,37 @@
+// You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+// Example 1:
+// Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+// Output: true
+// Example 2:
+// Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+// Output: false
+ 
+// Constraints:
+
+// 2 <= coordinates.length <= 1000
+// coordinates[i].length == 2
+// -10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+// coordinates contains no duplicate point.
+
+//=============================================================================================
+/**
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+function slop (x1 , y1 , x2 , y2){
+  if(x2 - x1 === 0){
+      return 2;
+  }
+  return (y2-y1)/(x2-x1);
+}
+
+var checkStraightLine = function(co) {
+  let m = slop(co[0][0] , co[0][1]  , co[1][0] , co[1][1]);
+  for(let i=2 ; i< co.length ; i++){
+      if(m !== slop(co[0][0] , co[0][1]  , co[i][0] , co[i][1])){
+          return false;
+      }
+  }
+  return true;
+};
