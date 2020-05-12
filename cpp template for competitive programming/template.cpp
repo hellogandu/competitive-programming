e0b9ba38fb4822c32f87aba67fc21040a67e2f29
reettik <reettikgoswami1997@gmail.com>
2020-05-12 11:55:16 +0530

    add cpp template folder

---
+++ cpp template for competitive programming/template.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int , int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for (int i = a; i <= b; i++)
+
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+ #ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+ #endif
+
+ return 0
+}
