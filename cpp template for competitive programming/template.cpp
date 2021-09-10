313632bdb7a5ec90fb193ad7c8872259054fd987
reettik97 <reettikgoswami1997@gmail.com>
2021-09-10 12:38:10 +0530

    Update CP template

--- cpp template for competitive programming/template.cpp
+++ cpp template for competitive programming/template.cpp
@@ -14,9 +14,12 @@ void solve(){
 }
 
 int main(){
-ios::sync_with_stdio(0);
-cin.tie(0);
-
-solve();
-return 0;
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T = 1;
+  cin >> T;
+  while(T--){
+    solve();
+  }
+  return 0;
 }
