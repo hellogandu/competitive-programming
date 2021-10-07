ea04e31b5a44274d15bab5cddda764cf46a8d10a
reettik97 <reettikgoswami1997@gmail.com>
2021-10-07 21:39:11 +0530

    A. Word Capitalization

---
+++ codeforces/practice(<1399)/A_Word_Capitalization.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int , int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for (int i = a; i <= b; i++)
+
+void solve(){
+  string s;
+  cin >> s;
+  int a = s[0];
+  if(a >= 97 && a<=122){
+    s[0] = a - 32;
+  }
+  cout << s;
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T = 1;
+  // cin >> T;
+  while(T--){
+    solve();
+  }
+  return 0;
+}
