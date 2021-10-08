75120e643401d15faa682c2948aa438cea1bff94
reettik97 <reettikgoswami1997@gmail.com>
2021-10-08 23:32:22 +0530

    B - Special Numbers

---
+++ codeforces/practice(<1399)/B_Special_Numbers.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int , int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for (int i = a; i <= b; i++)
+
+ll mod = 1000000000 + 7;
+
+int power(ll x, ll y, ll p)
+{
+    int res = 1;     // Initialize result
+ 
+    x = x % p; // Update x if it is more than or
+                // equal to p
+  
+    if (x == 0) return 0; // In case x is divisible by p;
+ 
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res*x) % p;
+ 
+        // y must be even now
+        y = y>>1; // y = y/2
+        x = (x*x) % p;
+    }
+    return res;
+}
+
+void solve(){
+  ll n, k, ans = 0, count = 0;
+  cin >> n>>k;
+  while(k){
+    if(k & 1){
+      ans = ( ans + power(n, count, mod) )% mod;
+    }
+    count++;
+    k = k >> 1;
+  }
+  cout << ans << endl;
+}
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T = 1;
+  cin >> T;
+  while(T--){
+    solve();
+  }
+  return 0;
+}
