09441ca8c5fd7104c623a058a4737862fd04b818
reettik97 <reettikgoswami1997@gmail.com>
2021-10-08 23:32:11 +0530

    A - Consecutive Sum Riddle

---
+++ codeforces/practice(<1399)/A_Consecutive_Sum_Riddle.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int , int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for (int i = a; i <= b; i++)
+
+void solve(){
+  ll n;
+  cin >> n;
+  cout << (n - 1) * (-1) << " " << n << endl;
+  
+}
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T = 1;
+  cin >> T;
+  while(T--){
+    solve();
+  }
+  return 0;
+}
