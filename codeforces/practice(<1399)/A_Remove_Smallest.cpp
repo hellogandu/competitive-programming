cf9e17c5d05d3975ae8dff4477b4072addddba88
reettik97 <reettikgoswami1997@gmail.com>
2021-10-11 23:02:26 +0530

    A. Remove Smallest

---
+++ codeforces/practice(<1399)/A_Remove_Smallest.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int , int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for (int i = a; i <= b; i++)
+
+void solve(){
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  loop(i,0,n-1){
+    cin >> v[i];
+  }
+  sort(v.begin(), v.end());
+  loop(i,0,n-2){
+    if(v[i+1] - v[i] > 1){
+      cout << "NO" << endl;
+      return;
+    }
+  }
+  cout << "YES" << endl;
+
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T = 1;
+  cin >> T;
+  while(T--){
+    solve();
+  }
+  return 0;
+}
