4ed1f29d7dafe63120fee58e6f68d11fc3854830
reettik97 <reettikgoswami1997@gmail.com>
2021-10-18 21:54:06 +0530

    581A - Vasya the Hipster

---
+++ codeforces/practice(<1399)/A_Vasya_the_Hipster.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int , int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for (int i = a; i <= b; i++)
+
+void solve(){
+  int a, b;
+  cin >> a >> b;
+  cout << min(a, b) << " " << abs(a - b) / 2;
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T = 1;
+  // cin >> T;
+  while(T--){
+    solve();
+  }
+  return 0;
+}
