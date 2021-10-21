ae83977fa85755b9515fb8e38429bd1ba358ed51
reettik97 <reettikgoswami1997@gmail.com>
2021-10-21 21:28:49 +0530

    1030A - In Search of an Easy Problem

---
+++ codeforces/practice(<1399)/A_In_Search_of_an_Easy_Problem.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int , int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for (int i = a; i <= b; i++)
+
+void solve(){
+  int n;
+  cin >> n;
+  while(n--){
+    int t;
+    cin >> t;
+    if(t){
+      cout << "HARD";
+      return;
+    }
+  }
+  cout << "EASY";
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T = 1;
+  // cin >> T;
+  while(T--){
+    solve();
+  }
+  return 0;
+}
