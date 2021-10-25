8c805e972fb6a361f804aa764bdf93bd9cfaf014
reettik97 <reettikgoswami1997@gmail.com>
2021-10-25 21:34:08 +0530

    977A - Wrong Subtraction

---
+++ codeforces/practice(<1399)/A_Wrong_Subtraction.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int , int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for (int i = a; i <= b; i++)
+
+void solve(){
+  int n, k;
+  cin >> n >> k;
+  while(k--){
+    if(n%10==0){
+      n = n / 10;
+    }else{
+      n -= 1;
+    }
+  }
+  cout << n;
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T = 1;
+  // cin >> T;
+  while(T--){
+    solve();
+  }
+  return 0;
+}
