6027221e1f5f5e2a14a2aa3d296369ee8fcdca29
reettik97 <reettikgoswami1997@gmail.com>
2021-10-29 22:01:10 +0530

    791A - Bear and Big Brother

---
+++ codeforces/practice(<1399)/A_Bear_and_Big_Brother.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int , int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for (int i = a; i <= b; i++)
+
+void solve(){
+  int a, b,c=0;
+  cin >> a >> b;
+  while (a <= b)
+  {
+    a *= 3;
+    b *= 2;
+    c++;
+  }
+  cout << c;
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T = 1;
+  // cin >> T;
+  while(T--){
+    solve();
+  }
+  return 0;
+}
