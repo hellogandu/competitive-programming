606368c69b9331aa6d385fa03931697bf3b248f1
reettik97 <reettikgoswami1997@gmail.com>
2021-11-04 22:36:47 +0530

    677A - Vanya and Fence

---
+++ codeforces/practice(<1399)/A_Vanya_and_Fence.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int , int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for (int i = a; i <= b; i++)
+
+void solve(){
+  int n, h, c=0;
+  cin >> n >> h;
+  while (n--)
+  {
+    int t;
+    cin >> t;
+    if(t>h){
+      c+=2;
+    }else{
+      c++;
+    }
+  }
+  cout << c;
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T = 1;
+  // cin >> T;
+  while(T--){
+    solve();
+  }
+  return 0;
+}
