f0e83ca06884b31527675afa6edd365caee1ded0
reettik97 <reettikgoswami1997@gmail.com>
2021-11-07 22:28:36 +0530

    486A - Calculating Function

---
+++ codeforces/practice(<1399)/A_Calculating_Function.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int , int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define loop(i,a,b) for (int i = a; i <= b; i++)
+
+void solve(){
+  ll n;
+  cin >> n;
+  if(n%2==0){
+    cout << n / 2;
+  }else{
+    cout << n * -1 + n / 2;
+  }
+}
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T = 1;
+  // cin >> T;
+  while(T--){
+    solve();
+  }
+  return 0;
+}
